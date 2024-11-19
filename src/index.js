80b0dd98d254a845a39a4293a525b14040ddfd36
anniesalis <anniesalis02@gmail.com>
2024-11-19 00:50:35 +0000

    Added theme to homepage

--- src/index.js
+++ src/index.js
@@ -15,9 +15,24 @@ let typewriter = new Typewriter(app, {
 typewriter
   .typeString('<span style="color: powderblue; ">Hi, I am Annie Salis')
   .pauseFor(300)
-  .typeString('<br> <span style="color: blueviolet;"><h2> FrontEnd Engineer || Virtual Assistant')
+  .typeString('<br> <span style="color: var(--primary-color);"><h2> FrontEnd Engineer || Virtual Assistant')
   .pauseFor()
   .start();
 
+  function changeTheme() {
+    let body = document.querySelector("body");
 
+    // body.classList.toggle("dark");
 
+    if (body.classList.contains("dark")) {
+      body.classList.remove("dark");
+    } else {
+      body.classList.add("dark");
+    }
+  }
+
+  let themeButton = document.querySelector("button");
+  themeButton.addEventListener("click", changeTheme);
+
+  
+ 
