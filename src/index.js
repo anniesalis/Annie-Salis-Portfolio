2021e03ad678034be726db918c4b8a81083d4f6b
anniesalis <anniesalis02@gmail.com>
2025-01-01 14:15:42 +0000

    Added Animation for scroll effect.

--- src/index.js
+++ src/index.js
@@ -34,3 +34,22 @@ typewriter
   let themeButton = document.querySelector("button");
   themeButton.addEventListener("click", changeTheme);
 
+  gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray("div").forEach((section) => {
+    gsap.fromTo(
+        section,
+        { opacity: 0, y: 50 }, // Start state
+        {
+            opacity: 1,
+            y: 0,
+            duration: 1,
+            scrollTrigger: {
+                trigger: section,
+                start: "top 80%", // When 80% of the viewport hits the top of the section
+                end: "bottom 20%", // When 20% of the viewport leaves the section
+                toggleActions: "play none none none", // Play animation on scroll
+            },
+        }
+    );
+});
